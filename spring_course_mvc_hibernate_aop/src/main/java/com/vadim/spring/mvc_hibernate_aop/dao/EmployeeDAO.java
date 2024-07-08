5f99e6013dbc9fc5985d50dbdc8ab06184bc9fb8
M0nkeyda$h <94993549+M0nkeydaSh@users.noreply.github.com>
2024-07-08 16:57:39 +0300

    Add files via upload

---
+++ spring_course_mvc_hibernate_aop/src/main/java/com/vadim/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,10 @@
+package com.vadim.spring.mvc_hibernate_aop.dao;
+
+import com.vadim.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+}
